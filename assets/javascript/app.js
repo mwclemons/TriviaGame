3501f6c12d930efcaf4d43dba6373f230b7b9757
Michael Clemons <mwclemons@gmail.com>
2019-04-05 12:48:44 -0500

    just missing additional questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,190 @@
+var currentID;
+        var previousIDs = [];
+        var numWins = 0;
+        var numLoses = 0;
+        var numUnanswered = 0;
+        var timerRunning = false;
+        var timeLeft = 0;
+        var delayLeft = 0;
+        var intervalID;  
+        
+        
+        var triviaQuestions = [
+            {question:"Before the steroid era completely obliterated the MLB HR records in the late 90s, Roger Maris held the single season HR record at 61.  In what year did he accomplish this feat?", 
+                choices:["1956", "1983", "1961", "1972"], 
+                answer: "1961", 
+                image:"assets/images/maris.jpg"},
+            {question:"In the 1982 season finale on Monday Night Football, Tony Dorsett set an NFL record for the longest run in NFL history by scoring a 99 yard TD.  This record will never be broken.  What fellow Heisman winner was the first and only NFL player to tie this record?", 
+                choices:["Derrick Henry", "Ricky Williams", "Eddie George", "Mark Ingram"], 
+                answer: "Derrick Henry", 
+                image:"assets/images/henry.jpg"},
+            {question:"Who is the only person in NBA History to be named MVP, Coach of the Year and Executive of the Year?", 
+                choices:["Michael Jordan", "Bill Russell", "Larry Bird", "Phil Jackson"], 
+                answer: "Larry Bird", 
+                image:"assets/images/bird.jpg"},
+            {question:"What former major leaguer retired from baseball with exact same number of major league HRs as his father?", 
+                choices:["Prince Fielder", "Ken Griffey Jr.", "Cal Ripken Jr.", "Moises Alou"], 
+                answer: "Prince Fielder", 
+                image:"assets/images/prince.jpg"},
+            {question:"Who was Muhammad Ali's opponent in the Rumble in the Jungle?", 
+                choices:["Joe Fraizer", "George Foreman", "Ken Norton", "Trevor Berbick"], 
+                answer: "George Foreman", 
+                image:"assets/images/rumble.jpg"},
+            {question:"Only 7 players in NBA history have completed a season where they shot 50% on FGs, 90% on FTs and 40% on 3FGs.  Who is the only player to accomplish this in 4 different seasons?", 
+                choices:["Steve Nash", "Dirk Nowitzki", "Steph Curry", "Larry Bird"], 
+                answer: "Steve Nash", 
+                image:"assets/images/nash.jpg"},
+            {question:"Who is the only coach to win a championship in both the NBA and NCAA? ", 
+                choices:["Mike Krzyzewski", "Larry Brown", "Chuck Daly", "Rick Pitino"], 
+                answer: "Larry Brown", 
+                image:"assets/images/brown.jpg"},
+        ];
+
+        function pickNextQuestion() {
+            currentID = Math.floor(Math.random() * (triviaQuestions.length));
+            while (previousIDs.includes(currentID)){
+                currentID = Math.floor(Math.random() * (triviaQuestions.length));
+            };
+            previousIDs.push(currentID);
+            stopCountDown();
+            updateBoardNewQuestion(); 
+        };
+
+        function updateBoardNewQuestion() {
+            
+            $("#trivia-question").text(triviaQuestions[currentID].question);
+            //Randomize the order in which the answer choices appear on the page
+            randomizeChoices(triviaQuestions[currentID].choices) 
+            $("#question-image").empty();
+            resetCountDown(30);
+            startCountDown();
+        };
+
+        function updateBoardAnswer(myResult){
+            
+            $("#time-remaining").empty();
+            $("#trivia-question").empty();
+            $("#choice-0").empty();
+            $("#choice-1").empty();
+            $("#choice-2").empty();
+            $("#choice-3").empty();
+            if (myResult === "Wrong") {
+                numLoses++;
+                $("#trivia-question").text("Sorry that is incorrect. The correct answer is "+triviaQuestions[currentID].answer+".");
+            } else if (myResult === "Correct") {
+                numWins++;
+                $("#trivia-question").text("Congratulations!! That was the correct answer.");
+            } else if (myResult === "TimerExpired") {
+                numUnanswered++;
+                $("#trivia-question").text("Sorry, you ran out of time. The correct answer is "+triviaQuestions[currentID].answer+".");
+            };
+            $("#question-image").html("<img src='"+triviaQuestions[currentID].image+"' class='img-fluid rounded'>");
+              
+            stopCountDown();
+            startDelay();
+            
+            
+        };
+
+        function updateBoardGameOver() {
+
+            $("#start-button").css("visibility", "visible");
+            $("#start-button").text("Start Over?");
+            $("#question-image").empty();
+            $("#trivia-question").text("Game Over!! Here's how you did:");
+            $("#trivia-question").append("<br>Correct Answers: "+numWins);
+            $("#trivia-question").append("<br>Incorrect Answers: "+numLoses);
+            $("#trivia-question").append("<br>Unanswered: "+numUnanswered);
+                       
+        };
+
+
+        function randomizeChoices(myChoices) {
+            var currentChoice;
+            var previousChoices = [];
+            var x = 0;
+            while (previousChoices.length < myChoices.length) {
+                currentChoice = Math.floor(Math.random() * (myChoices.length));
+                while (previousChoices.includes(currentChoice)){
+                    currentChoice = Math.floor(Math.random() * (myChoices.length));
+                };
+                previousChoices.push(currentChoice);
+                $("#choice-"+x).text(myChoices[previousChoices[x]]);
+                x++;
+            };
+        };
+
+        function startDelay() {
+            delayLeft=5;
+            if (!timerRunning) {
+                intervalID = setInterval(countDownDelay, 1000);
+                timerRunning = true;
+            }
+        };
+
+        function countDownDelay() {
+            delayLeft--;
+            
+            
+            if (delayLeft < 0) {
+                if (numLoses+numWins+numUnanswered < 5) {
+                    stopCountDown();
+                    pickNextQuestion();
+                } else {
+                    stopCountDown();
+                    updateBoardGameOver();
+                };
+                
+            };
+        };
+
+        function startCountDown() {
+            if (!timerRunning) {
+                intervalID = setInterval(countDownQuestion, 1000);
+                timerRunning = true;
+            }
+        };
+
+        function stopCountDown() {
+            clearInterval(intervalID);
+            timerRunning = false;
+        };
+
+        function resetCountDown(countFrom) {
+            timeLeft = countFrom;
+            $("#time-remaining").text(timeLeft+" seconds remaining");
+        };
+
+        function countDownQuestion() {
+            timeLeft--;
+            if (timeLeft>=0) {
+                $("#time-remaining").text(timeLeft+" seconds remaining");
+            } else {
+                stopCountDown();
+                updateBoardAnswer("TimerExpired");   
+            };
+        };
+
+
+        
+
+        $(".trivia-choices").on("click", function(){
+            if ($(this).text() === triviaQuestions[currentID].answer) {
+                updateBoardAnswer("Correct");
+            } else {
+                updateBoardAnswer("Wrong");   
+            };
+        });
+
+
+        $("#start-button").on("click", function(){
+            numWins = 0;
+            numLoses = 0;
+            numUnanswered = 0;
+            timerRunning = false;
+            timeLeft = 0;
+            previousIDs = [];
+            $("#start-button").css("visibility", "hidden");
+            pickNextQuestion();
+            
+        });
